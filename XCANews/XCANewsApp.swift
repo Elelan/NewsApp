d6d61c5fad9e419c502d21a73082e833b1f0c617
elelan's mac <elelanv@gmail.com>
2022-03-17 23:39:45 +0530

    Initial Commit

---
+++ XCANews/XCANewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCANewsApp.swift
+//  XCANews
+//
+//  Created by Elelan on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct XCANewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
