bf1c731d95c557183b4ad1892541da37081cc08c
elelan's mac <elelanv@gmail.com>
2022-03-20 20:06:16 +0530

    added bookmark section, storing articles and search functionality

--- XCANews/Views/BookmarkTabView.swift
+++ XCANews/Views/BookmarkTabView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct BookmarkTabView: View {
+    
+    @EnvironmentObject var viewModel: BookmarkViewModel
+    @State var searchText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        let articles = self.articles
+        NavigationView {
+            ArticleListView(articles: articles)
+                .overlay(overlayView(isEmpty: articles.isEmpty))
+                .navigationTitle("Saved Articles")
+                .searchable(text: $searchText)
+        }
+    }
+    
+    private var articles: [Article] {
+        if searchText.isEmpty {
+            return viewModel.bookmarks
+        }
+        return viewModel.bookmarks.filter {
+            $0.title.lowercased().contains(searchText.lowercased()) ||
+            $0.descriptionText.lowercased().contains(searchText.lowercased())
+        }
+    }
+    
+    @ViewBuilder
+    func overlayView(isEmpty: Bool) -> some View {
+        if isEmpty {
+            EmptyPlaceholderView(text: "No saved Articles", image: Image(systemName: "bookmark"))
+        }
     }
 }
 
 struct BookmarkTabView_Previews: PreviewProvider {
+    
+    @StateObject static var viewModel = BookmarkViewModel.shared
+    
     static var previews: some View {
         BookmarkTabView()
+            .environmentObject(viewModel)
     }
 }
